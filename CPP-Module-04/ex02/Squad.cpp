f19f446eb851a7d6cbb2b8c391501c6796306d5c
42corozco <42corozco@gmail.com>
2021-03-12 22:51:14 +0100

    update module 04 -fin ex02

--- CPP-Module-04/ex02/Squad.cpp
+++ CPP-Module-04/ex02/Squad.cpp
@@ -1,4 +1,5 @@
 #include "Squad.hpp"
+#include <algorithm>
 
 Squad::Squad() : _count(0)
 {
@@ -14,23 +15,35 @@ Squad&	Squad::operator=( Squad const & rhs)
 	this->_count = rhs._count;
 	std::vector<ISpaceMarine*> tmp(rhs._unit);
 	this->_unit = tmp;
-//	this->_unit = rhs._unit;
+	//	this->_unit = rhs._unit;
 	return ( *this );
 }
 
 Squad::~Squad()
 {
+	for (int i = 0; i < (int)_unit.size(); i++)
+	{
+		this->_count--;
+		delete (_unit[i]);
+	}
+	_unit.clear();
 }
 
-int				Squad::getCount() const
+int		Squad::getCount() const
 {
 	return ( this->_count );
 }
 
-ISpaceMarine*	Squad::getUnit(int i) const //falta terminar
+ISpaceMarine*	Squad::getUnit(int i) const
 {
 	if (i < (int)_unit.size())
 		return (_unit[i]);
 	return (NULL);
 }
-//	 int				Squad::push(ISpaceMarine*);
+
+int		Squad::push(ISpaceMarine* obj)
+{
+	_unit.push_back(obj);
+	this->_count++;
+	return ((int)this->_unit.size());
+}
--- CPP-Module-04/ex02/main.cpp
+++ CPP-Module-04/ex02/main.cpp
@@ -8,7 +8,8 @@ int main()
 	ISpaceMarine* jim = new AssaultTerminator;
 
 	ISquad* vlc = new Squad;
-	vlc->push(bob);vlc->push(jim);
+	vlc->push(bob);
+	vlc->push(jim);
 	for (int i = 0; i < vlc->getCount(); ++i)
 	{
 		ISpaceMarine* cur = vlc->getUnit(i);
