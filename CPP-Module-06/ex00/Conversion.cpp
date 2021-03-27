ef3747fddeccff043eda60bfac29b0f843e84763
42corozco <42corozco@gmail.com>
2021-03-27 22:05:49 +0100

    update module 06 -re hacer todo el int  ex00

--- CPP-Module-06/ex00/Conversion.cpp
+++ CPP-Module-06/ex00/Conversion.cpp
@@ -40,7 +40,7 @@ void	Conversion::convertToChar() const
 			num = static_cast<int>(this->_number[0]);
 	}
 	if (!std::strncmp(this->_number.c_str(), "nan", 3) \
-		|| !(num > -129 && num < 128))
+			|| !(num > -129 && num < 128))
 		std::cout << "Impossible";
 	else if (std::isprint(static_cast<char>(num)))
 		std::cout << "'" << static_cast<char>(num) << "'";
@@ -54,6 +54,7 @@ void	Conversion::convertToInt() const
 	int	num(0);
 
 	std::cout << "int: ";
+	std::istringstream ( this->_number ) >> num;
 	if (!std::strncmp(this->_number.c_str(), "inf", 3) || !std::strncmp(this->_number.c_str(), "+inf", 4))
 	{
 		std::cout << "inf" << std::endl;
@@ -64,21 +65,15 @@ void	Conversion::convertToInt() const
 		std::cout << "-inf" << std::endl;
 		return ;
 	}
-	try
+	else if ((this->_number.size() > 1 && num == 0) \
+	|| !std::strncmp(this->_number.c_str(), "nan", 3))
 	{
-		//num = static_cast<int>(this->_number[0]);
-		if (this->_number.size() == 1 && !(this->_number[0] >= '0' && this->_number[0] <= '9'))
-			num = static_cast<int>(this->_number[0]);
-		else
-			std::istringstream ( this->_number ) >> num;
-		//	num = std::atoi(static_cast<const char*>(this->_number.c_str()));
-		std::cout << num;
+		std::cout << "Impossible" << std::endl;
+		return ;
 	}
-	catch (std::exception & e)
-	{
-		std::cout << "Impossible";
-	}
-	std::cout << std::endl;
+	else if (this->_number.size() == 1 && !(this->_number[0] >= '0' && this->_number[0] <= '9'))
+		num = static_cast<int>(this->_number[0]);
+	std::cout << num << std::endl;
 }
 
 void	Conversion::convertToFloat() const
