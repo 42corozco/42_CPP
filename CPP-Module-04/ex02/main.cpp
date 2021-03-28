35730478028a76e9fb49907fab7bf58b75ed98e6
42corozco <corozco@student.42.fr>
2021-03-28 21:31:54 +0200

    fin module 04 -add test2 ex02

--- CPP-Module-04/ex02/main.cpp
+++ CPP-Module-04/ex02/main.cpp
@@ -3,21 +3,45 @@
 #include "Squad.hpp"
 #include <unistd.h>
 
+#define COLOR_ONE	"\033[35m"
+#define COLOR_TWO	"\033[33m"
+#define COLOR_NO	"\033[0m"
+
 int main()
 {
-	ISpaceMarine* bob = new TacticalMarine();
-	ISpaceMarine* jim = new AssaultTerminator();
-
-	ISquad* vlc = new Squad();
-	vlc->push(bob);
-	vlc->push(jim);
-	for (int i = 0; i < vlc->getCount(); ++i)
 	{
-		ISpaceMarine* cur = vlc->getUnit(i);
-		cur->battleCry();
-		cur->rangedAttack();
-		cur->meleeAttack();
+		std::cout << COLOR_TWO << "------------test1----------------" << COLOR_NO << std::endl;
+		ISpaceMarine* bob = new TacticalMarine();
+		ISpaceMarine* jim = new AssaultTerminator();
+
+		ISquad* vlc = new Squad();
+		vlc->push(bob);
+		vlc->push(jim);
+		for (int i = 0; i < vlc->getCount(); ++i)
+		{
+			ISpaceMarine* cur = vlc->getUnit(i);
+			cur->battleCry();
+			cur->rangedAttack();
+			cur->meleeAttack();
+		}
+		std::cout << "--------------destructors--------------" << std::endl;
+		delete vlc;
+		delete bob;
+		delete jim;
 	}
-	delete vlc;
+	{
+		std::cout << COLOR_TWO << "------------test2----------------" << COLOR_NO << std::endl;
+		ISpaceMarine* bob = new TacticalMarine();
+		std::cout << "--------------Constructors--------------" << std::endl;
+
+		bob->battleCry();
+		std::cout << "Clone" << std::endl;
+		ISpaceMarine* nani;
+		nani = bob->clone();
+		nani->rangedAttack();
+		std::cout << "--------------destructors--------------" << std::endl;
+		delete bob;
+		delete nani;
+	}
 	return 0;
 }
