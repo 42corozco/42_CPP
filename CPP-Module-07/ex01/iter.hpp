2544f693910cfaed5e5507878d48a2265454fe43
42corozco <corozco@student.42.fr>
2021-04-01 15:05:23 +0200

    update module 07 -update ex01

--- CPP-Module-07/ex01/iter.hpp
+++ CPP-Module-07/ex01/iter.hpp
@@ -4,8 +4,8 @@
 # include <iostream>
 # include <string>
 
-template < typename T, typename Ti>
-void	iter( T * tab, Ti const & i, void f(T const &tab) )
+template < typename T>
+void	iter( T * tab, T const & i, void f(T const &tab) )
 {
 	for (int k = 0; k < i; k++)
 		f(tab[k]);
--- CPP-Module-07/ex01/main.cpp
+++ CPP-Module-07/ex01/main.cpp
@@ -5,13 +5,13 @@ int	main(void)
 	{
 		int tab[] = {0, 1, 2, 3};
 
-		iter< int, int >(tab, 4, shout);
-		iter< int, int >(tab, 2, shout);
+		iter< int >(tab, 4, shout);
+		iter< int >(tab, 2, shout);
 	}
 	{
-		int tab[] = {0, 1, 2, 3};
+//		int tab[] = {0, 1, 2, 3};
 
-		iter< int, int >(tab, 9, shout);
+//		iter< int >(tab, 9, shout);
 	}
 	return (0);
 }
