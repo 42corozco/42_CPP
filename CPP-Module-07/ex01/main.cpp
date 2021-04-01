4b4848a94a1557934c1bb98dd515ab27701954eb
42corozco <42corozco@gmail.com>
2021-04-01 22:51:16 +0200

    update module 06 -fin ex01

--- CPP-Module-07/ex01/main.cpp
+++ CPP-Module-07/ex01/main.cpp
@@ -5,13 +5,18 @@ int	main(void)
 	{
 		int tab[] = {0, 1, 2, 3};
 
+		std::cout << "test-> iter< char >(tab, 4, shout);" << std::endl;
 		iter< int >(tab, 4, shout);
+		std::cout << "test-> iter< char >(tab, 2, shout);" << std::endl;
 		iter< int >(tab, 2, shout);
 	}
 	{
-//		int tab[] = {0, 1, 2, 3};
+		char tab[] = {'a', 'b', 'c', 'd'};
 
-//		iter< int >(tab, 9, shout);
+		std::cout << "test-> iter< char >(tab, 4, shout);" << std::endl;
+		iter< char >(tab, 4, shout);
+		std::cout << "test-> iter< char >(tab, 2, shout);" << std::endl;
+		iter< char >(tab, 4, shout);
 	}
 	return (0);
 }
