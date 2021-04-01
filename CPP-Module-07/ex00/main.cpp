6c4d2379c496a69e89827765eaf6cd3e9fe0a04c
42corozco <corozco@student.42.fr>
2021-04-01 13:30:11 +0200

    update module 07 -fin ex00

--- CPP-Module-07/ex00/main.cpp
+++ CPP-Module-07/ex00/main.cpp
@@ -4,27 +4,22 @@
 int		main(void)
 {
 	{
-		int a(42);
-		int b(35);
+	int	a = 2;
+	int	b = 3;
 
-		std::cout << "a: " << a << " b: " << b << std::endl;
-		std::cout << max< int >( a, b ) << std::endl;
-		std::cout << min< int >( a, b ) << std::endl;
-		swap < int >(&a, &b);
-		std::cout << "a: " << a << " b: " << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	}
-	{
-		std::string b = "Nani";
-		std::string a = "Quimera";
-
-		std::cout << "a: " << a << " b: " << b << std::endl;
-		std::cout << ::max< std::string >( a, b ) << std::endl;
-		std::cout << ::min< std::string >( a, b ) << std::endl;
-		swap < std::string >(&a, &b);
-		std::cout << "a: " << a << " b: " << b << std::endl;
-	
-	}
-
 
 	return (0);
 }
--- CPP-Module-07/ex00/whatever.hpp
+++ CPP-Module-07/ex00/whatever.hpp
@@ -17,13 +17,13 @@ T const & min( T const & x, T const & y )
 }
 
 template < typename T>
-void	swap( T * x, T * y )
+void	swap( T & x, T & y )
 {
 	T	tmp;
 
-	tmp = *x;
-	*x = *y;
-	*y = tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
 }
 
 #endif
