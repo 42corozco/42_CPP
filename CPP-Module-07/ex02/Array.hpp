6461e7ff34d64611a04e01c62fede38c406b54cb
42corozco <corozco@student.42.fr>
2021-04-03 12:07:30 +0200

    update module 07 -update ex01

--- CPP-Module-07/ex02/Array.hpp
+++ CPP-Module-07/ex02/Array.hpp
@@ -8,16 +8,56 @@ template < typename T >
 class Array
 {
 	public:
-		Array();
-		Array(unsigned int _n);
-		~Array() { delete [] _Array; };
+		Array<T>()
+		{
+			this->_Array = new T[0];
+			this->_n = 0;
+		}
 
-		unsigned int	size() const { return this->_n; }
+		Array<T>(unsigned int n)
+		{
+			this->_Array = new T[n];
+			this->_n = n;
+		}
+
+		Array<T>( Array<T> const & obj)
+		{
+			*this = obj;
+		}
+
+		Array<T>& operator=( Array<T> const & obj)
+		{
+			this->_n = obj._n;
+			if ( this->_Array )
+				delete [] this->_Array;
+			this->_Array = new T[this->_n];
+			for (int i = 0; i < this->_n; i++)
+				this->_Array[i] = obj[i];
+			return ( *this );
+		}
+
+		~Array<T>()
+		{
+			if ( this->_Array )
+				delete [] this->_Array;
+		}
+
+		T & operator[](int index)
+		{
+			if (index >= this->_n)
+				throw std::exception(); 
+				//throw std::out_of_range("Error size"); 
+			return (this->_Array[index]);
+		}
+		
+
+		unsigned int	size() const { return ( this->_n ); }
 
 
 	protected:
 	private:
-		T * _Array;
+		T				*_Array;
+		unsigned int	_n;
 };
 
 #endif
