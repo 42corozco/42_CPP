79810020837435852776df9a66052a2748aa767d
42corozco <42corozco@gmail.com>
2021-04-07 23:21:09 +0200

    add module 08 -init ex00

--- CPP-Module-08/easyfind.hpp
+++ CPP-Module-08/easyfind.hpp
@@ -3,12 +3,26 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
+class	NotFoundException : public std::exception
+{
+	public:
+		NotFoundException() {}
+		virtual const char* what() const throw()
+		{
+			return ("Element not found");
+		}
+		virtual ~NotFoundException() throw() {}
+};
 
 template < typename T>
 void	easyfind(T & t, int i)
 {
-
+	if (t.end() != std::find(t.begin(), t.end(), i))
+		std::cout << "Element found" << std::endl;
+	else
+		throw NotFoundException();
 }
 
 #endif
--- CPP-Module-08/main.cpp
+++ CPP-Module-08/main.cpp
@@ -1,7 +1,25 @@
 #include "easyfind.hpp"
+#include <vector>
 
 int		main(void)
 {
+	std::vector<int> notes; //Un tableau vide
 
+	notes.push_back(12);  //On ajoute des cases avec les notes
+	notes.push_back(19);
+	notes.push_back(6);
+	notes.push_back(12);
+	notes.push_back(14);
+	notes.push_back(15);
+	easyfind(notes, 14);
+
+	try
+	{
+		easyfind(notes, 5);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return (0);
 }
